dc5a0e3af307939a833dc74afca88b1b76893846
Courtney Wilburn <courtney@cjwilburn.com>
2016-03-29 10:52:17 -0400

    adding lumen service provider

---
+++ src/Laravel/LumenServiceProvider.php
@@ -0,0 +1,57 @@
+<?php
+namespace PhantomPdf\Laravel;
+
+use PhantomPdf\PdfGenerator;
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class LumenServiceProvider extends BaseServiceProvider
+
+{
+    public function register()
+    {
+        $this->app->configure('phantom-pdf');
+
+        $configPath = __DIR__.'/../config/config.php';
+        $this->mergeConfigFrom($configPath, 'phantom-pdf');
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'phantom-pdf');
+
+        $this->app->singleton('phantom-pdf', function () {
+            $generator = new PdfGenerator;
+
+            $generator->setBaseUrl($this->app['config']['phantom-pdf.base_url'] ?: url('/'));
+
+            $generator->setBinaryPath($this->app['config']['phantom-pdf.binary_path']);
+
+            $generator->setStoragePath($this->app['config']['phantom-pdf.temporary_file_path']);
+
+            $generator->setTimeout($this->app['config']['phantom-pdf.timeout']);
+
+            foreach ($this->app['config']['phantom-pdf.command_line_options'] as $option) {
+                $generator->addCommandLineOption($option);
+            }
+
+            return $generator;
+        });
+
+        $this->app->alias('phantom-pdf', PdfGenerator::class);
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['phantom-pdf', PdfGenerator::class];
+    }
+}
